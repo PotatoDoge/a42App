b9dfa43e8d15fd081224a17ddbc69f766f8d9302
Bastian <bastian.prado.v@gmail.com>
2021-07-21 12:35:34 -0700

    - LogInActivity.kt has been created. Functionality is yet to be assigned - Changed status bar color

--- app/src/main/java/com/example/a42app/MainActivity.kt
+++ app/src/main/java/com/example/a42app/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.a42app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val logIn = findViewById<Button>(R.id.logIn_button)
+        logIn.setOnClickListener {
+            val intent = Intent(this,LogInActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
