6a2cf61f9e7a3b8463e5f7251b01eb89fdebb7f0
Bastian <bastian.prado.v@gmail.com>
2021-08-03 22:07:33 -0700

    - NavDrawer now is responsive - Added user's info at the top of the menu - Added fragments (both kt and xml files)

--- app/src/main/java/com/example/a42app/MenuScreenActivity.kt
+++ app/src/main/java/com/example/a42app/MenuScreenActivity.kt
@@ -3,14 +3,17 @@ package com.example.a42app
 import android.os.Bundle
 import android.view.MenuItem
 import android.widget.Toast
-import androidx.appcompat.widget.Toolbar
-import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.google.android.material.navigation.NavigationView
 
-class MenuScreenActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+open class MenuScreenActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var toggle: ActionBarDrawerToggle
@@ -28,6 +31,8 @@ class MenuScreenActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
         val nav = findViewById<NavigationView>(R.id.navigation_view)
         nav.setNavigationItemSelectedListener(this)
 
+        NavigationUI.setupWithNavController(nav, Navigation.findNavController(this,R.id.navHostFragment))
+
         toggle.syncState()
     }
 
@@ -44,8 +49,8 @@ class MenuScreenActivity : AppCompatActivity(), NavigationView.OnNavigationItemS
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
         when(item.itemId){
             R.id.menu1 -> Toast.makeText(this, "ITEM 1", Toast.LENGTH_SHORT).show()
-            R.id.menu2 -> Toast.makeText(this, "ITEM 1", Toast.LENGTH_SHORT).show()
-            R.id.menu3 -> Toast.makeText(this, "ITEM 1", Toast.LENGTH_SHORT).show()
+            R.id.menu2 -> Toast.makeText(this, "ITEM 2", Toast.LENGTH_SHORT).show()
+            R.id.menu3 -> Toast.makeText(this, "ITEM 3", Toast.LENGTH_SHORT).show()
         }
         drawerLayout.closeDrawer(GravityCompat.START)
         return true
